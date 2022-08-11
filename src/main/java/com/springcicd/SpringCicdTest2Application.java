64c919da9489c9425f9d6fd7559b4ad026fc5848
RajIncRev <rajkishore.behura@gmail.com>
2022-08-11 13:04:50 +0530

    initial checkin

---
+++ src/main/java/com/springcicd/SpringCicdTest2Application.java
@@ -0,0 +1,13 @@
+package com.springcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCicdTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCicdTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springcicd/SpringCicdTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCicdTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
